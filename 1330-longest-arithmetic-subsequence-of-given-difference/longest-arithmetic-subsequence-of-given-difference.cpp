3bb7506b23c075aa692bcfff9082fff8c78ed88e
shivansh2741 <76680346+shivansh2741@users.noreply.github.com>
2023-08-09 12:08:28 +0530

    Time: 189 ms (17.10%) | Memory: 57.6 MB (8.27%) - LeetSync

--- 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.cpp
@@ -8,14 +8,11 @@ public:
 
         for(int i=0;i<n;i++){
             int ans = 1;
-            if(mp.find(nums[i] - difference) != mp.end()){
-                ans = 1 + mp[nums[i] - difference];  
-                res = max(res , ans);  
-            }
+
+            ans = 1 + mp[nums[i] - difference];  
+            res = max(res , ans);  
             
-            if(mp.find(nums[i]) != mp.end())
-                mp[nums[i]] = max(mp[nums[i]] , ans);
-            else mp[nums[i]] = ans;
+            mp[nums[i]] = ans;
         }
 
         return res;
