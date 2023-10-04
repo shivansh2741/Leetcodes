5575a8726ecd3324f05955b79dbda6c7c866c034
shivansh2741 <76680346+shivansh2741@users.noreply.github.com>
2023-10-04 16:18:15 +0530

    Time: 1273 ms (63.63%) | Memory: 244.5 MB (29.39%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -4,12 +4,12 @@ public:
         int n = nums.size();
 
         vector<unordered_map<int,int>> v(n);
-
+        
         int ans = 2;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                int diff = nums[j] - nums[i];
+                int diff = nums[i] - nums[j];
 
                 if(v[j].find(diff) != v[j].end()){
                     v[i][diff] = 1 + v[j][diff];
