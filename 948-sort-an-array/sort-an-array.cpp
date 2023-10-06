934d6f9452e2ef0c7ba54622c0017e3f78fcec2c
shivansh2741 <76680346+shivansh2741@users.noreply.github.com>
2023-10-06 09:34:41 +0530

    Time: 179 ms (59.95%) | Memory: 67 MB (73.40%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,49 +1,36 @@
 class Solution {
 public:
-    void mergeSorted(vector<int>& nums,int lo,int mid,int hi){
-        int i = lo , j = mid+1;
+    void heapify(vector<int>& nums,int i,int n){
+        int largest = i;
+        int left = 2*i + 1 , right = 2*i + 2;
 
-        vector<int> temp;
+        if(left < n && nums[left] > nums[largest])
+            largest = left;
+        
+        if(right < n && nums[right] > nums[largest])
+            largest = right;
 
-        while(i <= mid && j <= hi){
-            if(nums[i] > nums[j]){
-                temp.push_back(nums[j]);
-                j++;
-            }
-            else{
-                temp.push_back(nums[i]);
-                i++;
-            }
-        }
-
-        while(i <= mid){
-            temp.push_back(nums[i]);
-            i++;
-        }
-
-        while(j <= hi){
-            temp.push_back(nums[j]);
-            j++;
-        }
-
-        for(int k=lo;k<=hi;k++){
-            nums[k] = temp[k-lo];
+        if(largest != i){
+            swap(nums[largest] , nums[i]);
+            heapify(nums,largest,n);
         }
     }
-    void merge(vector<int>& nums,int lo,int hi){
-        if(lo == hi)return;
 
-        int mid = (lo + hi)/2;
-        merge(nums,lo,mid);
-        merge(nums,mid+1,hi);
-
-        mergeSorted(nums,lo,mid,hi);
+    void buildHeap(vector<int> &nums){
+        int n = nums.size();
+        for(int i=n/2;i>=0;i--){
+            heapify(nums,i,n);
+        } 
     }
     vector<int> sortArray(vector<int>& nums) {
         int n = nums.size();
 
-        merge(nums,0,n-1);    
+        buildHeap(nums);
 
+        for(int i=n-1;i>=0;i--){
+            swap(nums[i] , nums[0]);
+            heapify(nums,0,i);
+        }
         return nums;
     }
 };
