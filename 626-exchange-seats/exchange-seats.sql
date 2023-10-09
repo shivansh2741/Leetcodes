77bb9cf3deaa29e618d5e075c8eec2c00386b8f1
shivansh2741 <76680346+shivansh2741@users.noreply.github.com>
2023-10-09 12:38:26 +0530

    Time: 891 ms (33.42%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+with t1 as(
+  select *,
+  lag(student) over(order by id) as prev_student,
+  lead(student) over(order by id) as next_student
+  from Seat
+)
+
+select id , coalesce(if(id%2 = 0 ,prev_student, next_student) , student) as student
+from t1
