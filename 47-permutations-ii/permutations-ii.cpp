81e4074fd26ac1026367287151b230a5493db183
shivansh2741 <76680346+shivansh2741@users.noreply.github.com>
2023-10-15 13:06:03 +0530

    Time: 53 ms (24.25%) | Memory: 10.1 MB (38.86%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     
-    void solve(vector<int>& nums,int index,
-              vector<vector<int>> &res){
-        
-        if(index==nums.size()){
-            res.push_back(nums);
+    void solve(vector<int>& nums,int n,set<vector<int>> &st,int i){
+
+        if(i == n){
+            st.insert(nums);
             return;
         }
-        
-        set<int> s;
-        for(int i=index;i<nums.size();i++){
-            if(s.find(nums[i])!=s.end())continue;
-            s.insert(nums[i]);
-            swap(nums[i],nums[index]);
-            solve(nums,index+1,res);
-            swap(nums[i],nums[index]);
+
+
+        // for ith index
+        for(int j=i;j<n;j++){
+            swap(nums[i] , nums[j]);
+            solve(nums,n,st,i+1);
+            swap(nums[i] , nums[j]);
         }
     }
     
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<int> temp;
-        vector<vector<int>> res;
-        
-        // sort(nums.begin(),nums.end());
         int n = nums.size();
-        solve(nums,0,res);
-        
-        return res;
+
+        vector<vector<int>> res;
+        set<vector<int>> st;
+
+        solve(nums,n,st,0);
+
+        for(auto it:st){
+            res.push_back(it);
+        }
+
+        return res;   
     }
 };
