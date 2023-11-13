2336f09e47ad88c53e64ae6a347c53098ead17d9
shivansh2741 <76680346+shivansh2741@users.noreply.github.com>
2023-11-13 19:44:33 +0530

    Time: 386 ms (87.50%) | Memory: 145 MB (6.25%) - LeetSync

--- 3197-maximum-strong-pair-xor-ii/maximum-strong-pair-xor-ii.cpp
+++ 3197-maximum-strong-pair-xor-ii/maximum-strong-pair-xor-ii.cpp
@@ -82,27 +82,28 @@ public:
     int maximumStrongPairXor(vector<int>& nums) {
         int n = nums.size();
         
-        sort(nums.begin() , nums.end());
+        sort(nums.begin() , nums.end()); //nlogn
         int maxAns = 0;
 
         Trie* obj = new Trie();
 
         int i = 0, j = 0;
 
-        while(i < n){
-            int index = upper_bound(nums.begin(),nums.end(),nums[i]*2) - nums.begin();
+        while(i < n){ //n
+            int index = upper_bound(nums.begin(),nums.end(),nums[i]*2) - nums.begin(); //logn
 
             index--;
             
-            while(j <= index){
-                obj->insert(nums[j]);
+            while(j <= index){  //--> O(n) overall
+                obj->insert(nums[j]);  //O(m)  m = length of word
                 j++;
             }
 
             maxAns = max(maxAns , obj->maxXor(nums[i]));
-            obj->remove(nums[i]);
+            obj->remove(nums[i]); // O(m)
             i++;
         }
         return maxAns;
     }
 };
+// TC - O(n*(logn + m) + n)
