bdd774189b26fb3f18159a15923bffb915ec6a1e
shivansh2741 <76680346+shivansh2741@users.noreply.github.com>
2023-12-08 09:27:28 +0530

    Time: 168 ms (66.95%) | Memory: 67.1 MB (79.24%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     void heapify(vector<int>& nums,int i,int n){
+        int l = 2*i + 1 , r = 2*i + 2;
         int largest = i;
 
-        int left = 2*i + 1;
-        int right = 2*i + 2;
-
-        if(left < n && nums[left] > nums[largest]){
-            largest = left;
-        }
-        if(right < n && nums[right] > nums[largest]){
-            largest = right;
-        }
-
+        if(l < n && nums[largest] < nums[l])
+            largest = l;
+        
+        if(r < n && nums[largest] < nums[r])
+            largest = r;
+        
         if(largest != i){
-            swap(nums[i] , nums[largest]);
+            swap(nums[largest] , nums[i]);
             heapify(nums,largest,n);
         }
     }
-    void buildheap(vector<int>& nums,int i,int n){
+
+    void buildheap(vector<int>& nums,int n){
         for(int i=n/2;i>=0;i--){
             heapify(nums,i,n);
         }
@@ -26,7 +24,15 @@ public:
     vector<int> sortArray(vector<int>& nums) {
         int n = nums.size();
 
-        buildheap(nums,0,n);
+        // buildheap(nums,n);
+
+        // for(int i=n-1;i>=0;i--){
+        //     heapify(nums,i,n);
+        // }
+
+        // reverse(nums.begin(), nums.end());
+        // return nums;
+        buildheap(nums,n);
 
         for(int i=n-1;i>=0;i--){
             swap(nums[0] , nums[i]);
